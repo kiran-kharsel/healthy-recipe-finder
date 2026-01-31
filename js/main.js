708f1ddcc517a49ef9e8c934ea5139ec439a6f91
kiran-kharsel <kirankharsel@gmail.com>
2026-01-31 15:37:27 +0530

    feat(js): add toggle functionality to hamburger menu

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// toggle nav element
+const hamburgerBtn = document.querySelector('.hamburger-menu')
+const navLinkElem = document.querySelector('.nav-link')
+
+hamburgerBtn.addEventListener('click', function(){
+    navLinkElem.classList.toggle('hidden')
+})
