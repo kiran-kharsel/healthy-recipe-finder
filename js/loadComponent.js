c6a1bf039634c20a8af35b18cfda9700ae27c149
kiran-kharsel <kirankharsel@gmail.com>
2026-02-01 15:04:01 +0530

    feat: create re-usable component for header and footer elements

---
+++ js/loadComponent.js
@@ -0,0 +1,17 @@
+async function loadSnippet(id, file) {
+  console.log(file);
+  const response = await fetch(file);
+  const html = await response.text();
+  document.getElementById(id).innerHTML = html;
+
+  // toggle nav menu
+  const hamburgerBtn = document.querySelector(".hamburger-menu");
+  const navLinkElem = document.querySelector(".nav-link");
+
+  hamburgerBtn.addEventListener("click", function () {
+    navLinkElem.classList.toggle("hidden");
+  });
+}
+
+// load header and footer
+loadSnippet("header", "components/header.html");
