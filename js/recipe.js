a7e26ca885fb2ab2686521a86826385b4931bbf9
kiran-kharsel <kirankharsel@gmail.com>
2026-02-02 16:19:21 +0530

    feat(recipe-js): fetch api call and show recipe list in ui

---
+++ js/recipe.js
@@ -0,0 +1,60 @@
+// data array
+let recipeList = [];
+
+const recipeListElem = document.querySelector('.recipe-list')
+
+
+
+// api call to get data
+async function getRecipe() {
+  try {
+    // Await the API call
+    const response = await fetch("https://dummyjson.com/recipes");
+
+    // Check if response is OK
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    // Parse JSON
+    const data = await response.json();
+
+    // Use the data
+    recipeList = [...data.recipes]
+    loadRecipeList(recipeList)
+
+  } catch (error) {
+    // Handle errors
+    console.error("Error fetching meals:", error);
+  }
+}
+
+getRecipe();
+
+
+// function to display recipes list
+function loadRecipeList(list){
+    console.log(list)
+    list.forEach((item) => {
+        const div = document.createElement('div')
+        div.classList.add('recipe-card', 'flex')
+        // or div.className = 'recipe-card flex';
+        
+        div.innerHTML = `
+        <div class="recipe-img">
+            <img src=${item.image} alt="">
+        </div>
+        <h4 class="recipe-title">${item.name}</h4>
+        <p class="recipe-desc">this is desc....</p>
+        <p class="serving"><i class="fa-solid fa-circle-user"></i> Serving: ${item.servings}</p>
+        <div class="time">
+            <p class="prep-time"><i class="fa-solid fa-alarm-clock"></i> Prep time: ${item.prepTimeMinutes}min</p>
+            <p class="cook-time"><i class="fa-solid fa-utensils"></i> Cook time: ${item.cookTimeMinutes}min</p>
+        </div>
+        <button class="view-recipe">view recipe</button>
+        `;
+        recipeListElem.appendChild(div)
+
+    });
+}
+
