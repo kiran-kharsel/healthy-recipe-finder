f2b079777279454c853f454b54e17a11921a3b62
kiran-kharsel <kirankharsel@gmail.com>
2026-02-06 15:44:20 +0530

    fix: correct flexbox alignment for child elements

--- js/loadComponent.js
+++ js/loadComponent.js
@@ -8,33 +8,9 @@ async function loadSnippet(id, file, callback) {
 
   if (typeof callback === "function") callback();
 
-
-  // toggle nav menu
-  // const hamburgerBtn = document.querySelector(".hamburger-menu");
-  // const navLinkElem = document.querySelector(".nav-link");
-
-  // if (hamburgerBtn && navLinkElem) {
-  //   hamburgerBtn.addEventListener("click", function () {
-  //     navLinkElem.classList.toggle("hidden");
-  //     console.log(navLinkElem);
-  //   });
-  // }
-  // document.addEventListener("click", (e) => {
-  // if (e.target.classList.contains("hamburger-menu")) {
-  //   console.log('ok')
-  //   const navLinkElem = document.querySelector(".nav-link");
-  //   if (navLinkElem) {
-  //     navLinkElem.classList.toggle("hidden");
-  //   }
-  // }
-  //});
 }
 
-// load header and footer
-// document.addEventListener("DOMContentLoaded", () => {
-//   loadSnippet("header", "components/header.html");
-//   loadSnippet("footer", "components/footer.html");
-// });
+
 
 loadSnippet("header", "components/header.html", initHeaderEvents);
 loadSnippet("footer", "components/footer.html");
