55edcf5eb772568feeb1d2da27ed78f5311eb17b
kiran-kharsel <kirankharsel@gmail.com>
2026-02-06 15:17:51 +0530

    refactor: ensure header events re-initilize after load

--- js/loadComponent.js
+++ js/loadComponent.js
@@ -1,17 +1,51 @@
-async function loadSnippet(id, file) {
+async function loadSnippet(id, file, callback) {
   const response = await fetch(file);
   const html = await response.text();
-  document.getElementById(id).innerHTML = html;
+  const container = document.getElementById(id);
+
+  if (!container) return;
+  container.innerHTML = html;
+
+  if (typeof callback === "function") callback();
+
 
   // toggle nav menu
-  const hamburgerBtn = document.querySelector(".hamburger-menu");
-  const navLinkElem = document.querySelector(".nav-link");
+  // const hamburgerBtn = document.querySelector(".hamburger-menu");
+  // const navLinkElem = document.querySelector(".nav-link");
 
-  hamburgerBtn.addEventListener("click", function () {
-    navLinkElem.classList.toggle("hidden");
-  });
+  // if (hamburgerBtn && navLinkElem) {
+  //   hamburgerBtn.addEventListener("click", function () {
+  //     navLinkElem.classList.toggle("hidden");
+  //     console.log(navLinkElem);
+  //   });
+  // }
+  // document.addEventListener("click", (e) => {
+  // if (e.target.classList.contains("hamburger-menu")) {
+  //   console.log('ok')
+  //   const navLinkElem = document.querySelector(".nav-link");
+  //   if (navLinkElem) {
+  //     navLinkElem.classList.toggle("hidden");
+  //   }
+  // }
+  //});
 }
 
 // load header and footer
-loadSnippet("header", "components/header.html");
+// document.addEventListener("DOMContentLoaded", () => {
+//   loadSnippet("header", "components/header.html");
+//   loadSnippet("footer", "components/footer.html");
+// });
+
+loadSnippet("header", "components/header.html", initHeaderEvents);
 loadSnippet("footer", "components/footer.html");
+
+function initHeaderEvents() {
+  const hamburgerBtn = document.querySelector(".hamburger-menu");
+  const navLinkElem = document.querySelector(".nav-link");
+
+  if (hamburgerBtn && navLinkElem) {
+    hamburgerBtn.addEventListener("click", () => {
+      navLinkElem.classList.toggle("hidden");
+    });
+  }
+}
