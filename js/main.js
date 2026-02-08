58126628df13ed3090a6707c0c0e7953e0840942
kiran-kharsel <kirankharsel@gmail.com>
2026-02-08 07:35:18 +0530

    chore: remove unnecessary JS file

--- js/main.js
+++
@@ -1,7 +0,0 @@
-// toggle nav element
-// const hamburgerBtn = document.querySelector('.hamburger-menu')
-// const navLinkElem = document.querySelector('.nav-link')
-
-// hamburgerBtn.addEventListener('click', function(){
-//     navLinkElem.classList.toggle('hidden')
-// })
