d5725a7f80544f0e0d3288033aa67b030ace95b4
kiran-kharsel <kirankharsel@gmail.com>
2026-02-08 07:26:00 +0530

    feat: add active-link class to current page

--- js/loadComponent.js
+++ js/loadComponent.js
@@ -18,10 +18,22 @@ loadSnippet("footer", "components/footer.html");
 function initHeaderEvents() {
   const hamburgerBtn = document.querySelector(".hamburger-menu");
   const navLinkElem = document.querySelector(".nav-link");
+  const navLinks = navLinkElem.querySelectorAll('.link')
 
   if (hamburgerBtn && navLinkElem) {
     hamburgerBtn.addEventListener("click", () => {
       navLinkElem.classList.toggle("hidden");
     });
-  }
+  };
+
+  // show active link
+  const currentPage = window.location.pathname.split("/").pop();
+
+  navLinks.forEach((link) => {
+    const lnkPage = link.getAttribute('href').split("/").pop();
+
+    if(currentPage === lnkPage){
+      link.classList.add('active-link')
+    }
+  })
 }
